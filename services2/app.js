4e74d2903e6ba0845b34c8675a116cd3cca038a7
arjit547 <97424724+arjit547@users.noreply.github.com>
2026-02-26 14:52:52 +0530

    Add files via upload

---
+++ services2/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Service 2 (Port 3001)");
+});
+
+app.listen(PORT, () => {
+  console.log(`Service 2 running on port ${PORT}`);
+});
