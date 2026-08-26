ce5e3329c39dec08fad574dfd129c1cc46334aca
arjit547 <97424724+arjit547@users.noreply.github.com>
2026-08-26 14:07:08 +0530

    Update response message in Service 1

--- service1/app.js
+++ service1/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Service 1 (Port 3000)");
+  res.send("Hello from  azure Service 1 (Port 3000)");
 });
 
 app.listen(PORT, () => {
